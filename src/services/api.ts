017480c157108811ec6e663ac0df70c700259271
Gabriel Pereira <gabriel9938@gmail.com>
2020-07-03 20:47:31 -0300

    add .env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://5efe4ad9dd373900160b40e1.mockapi.io/desafioAmbev/',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
